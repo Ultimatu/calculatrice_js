5653dd2ea05693dd06d7a7c33f948bc649705d5c
Ultimatu <tondesoulco@gmail.com>
2023-05-14 13:44:55 +0000

    first commit

---
+++ calculator.js
@@ -0,0 +1,34 @@
+function del() {
+  var value = document.getElementById("screen").value;
+  document.getElementById('screen').value = value.substr(0, value.length - 1);
+}
+
+var two = document.getElementById('two');
+two.addEventListener('click', function(){
+    var body = document.querySelector('body');
+    var toggle = document.getElementById('circle');
+    body.classList.add("active1");
+    body.classList.remove('active2');
+     body.classList.remove("activ0")
+    toggle.style.left = '36%';
+})
+
+var one = document.getElementById('one');
+one.addEventListener('click', function(){
+    var body = document.querySelector('body');
+    var toggle = document.getElementById("circle");
+    body.classList.remove('active1');
+    body.classList.remove("active2");
+    body.classList.add("activ0")
+    toggle.style.left = '0';
+})
+
+var three = document.getElementById("three");
+three.addEventListener("click", function(){
+    let body = document.querySelector("body");
+    let toggle = document.getElementById("circle");
+    body.classList.remove('active1');
+    body.classList.add("active2");
+    toggle.style.left = '70%'
+})
+
